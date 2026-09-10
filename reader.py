82ee0d18f9d55b7845aa9d40795412a0abd641eb
Matthew Green <mattgreen64@gmail.com>
2026-09-10 12:35:52 +0200

    5.3

--- reader.py
+++ reader.py
@@ -1,6 +1,7 @@
 import csv
 from collections.abc import Iterable, Callable
-
+from typing import Any
+from functools import partial
 
 def read_csv_as_dicts(filename: str, types: list[Callable[[str], object]]):
     '''
@@ -24,15 +25,10 @@ def csv_as_dicts(file: Iterable[str], types: list[Callable[[str], object]], head
 
     Optionally accepts headers for headerless CSVs.
     '''
-    records = []
-    rows = csv.reader(file)
-    if headers is None:
-        headers = next(rows)
-    for row in rows:
-        record = { name: func(val) 
-                   for name, func, val in zip(headers, types, row) }
-        records.append(record)
-    return records
+    def make_dict(headers, types, row):
+        return {name: func(val) for name, func, val in zip(headers, types, row)}
+    func = partial(make_dict, types=types)
+    return convert_csv(file, func)
 
 def csv_as_instances[T](file: Iterable[str], cls: type[T], headers: list[str] = None):
     '''
@@ -40,11 +36,15 @@ def csv_as_instances[T](file: Iterable[str], cls: type[T], headers: list[str] = 
 
     Optionally accepts headers for headerless CSVs.
     '''
+    def make_instance(row, cls, headers = None):
+        return cls.from_row(row)
+    func = partial(make_instance, cls=cls)
+    return convert_csv(file, func)
+
+def convert_csv(lines: Iterable[str], fn: Callable):
+    # This function shouldn't accept headers as an argument
+    # But how do we get the headers in then?
     records = []
-    rows = csv.reader(file)
-    if headers is None:
-        headers = next(rows)
-    for row in rows:
-        record = cls.from_row(row)
-        records.append(record)
-    return records
+    rows = csv.reader(lines)
+    headers = next(rows)
+    return list(map(lambda row: fn(headers=headers, row=row), rows))
