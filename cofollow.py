13751b342edb29c0fe6230b03a1cc7cfa6a3a402
Matthew Green <mattgreen64@gmail.com>
2026-09-17 10:27:35 +0200

    8.2 and 8.3

---
+++ cofollow.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+# Data source
+def follow(filename,target):
+    with open(filename,'r') as f:
+        f.seek(0,os.SEEK_END)
+        while True:
+            line = f.readline()
+            if line != '':
+                target.send(line)
+            else:
+                time.sleep(0.1)
+
+# Decorator for coroutine functions
+from functools import wraps
+
+def consumer(func):
+    @wraps(func)
+    def start(*args,**kwargs):
+        f = func(*args,**kwargs)
+        f.send(None)
+        return f
+    return start
+
+# Sample coroutine
+@consumer
+def printer():
+    while True:
+        item = yield     # Receive an item sent to me
+        print(item)
+
+if __name__ == '__main__':
+    follow('Data/stocklog.csv', printer())
