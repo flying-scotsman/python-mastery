60a24b54709642d0e8017d4a199f6ca7510a7fb3
Matthew Green <mattgreen64@gmail.com>
2026-09-20 21:11:01 +0200

    9.4

--- structly/tableformat/formatter.py
+++ structly/tableformat/formatter.py
@@ -1,31 +1,32 @@
 from abc import ABC, abstractmethod
 
 class TableFormatter(ABC):
+    _formats = { }
+
+    @classmethod
+    def __init_subclass__(cls):
+        name = cls.__module__.split('.')[-1]
+        TableFormatter._formats[name] = cls
+
     @abstractmethod
     def headings(self, headers):
-        raise NotImplementedError()
+        pass
 
     @abstractmethod
     def row(self, rowdata):
-        raise NotImplementedError()
+        pass
 
-from .formats.text import TextTableFormatter
-from .formats.csv import CSVTableFormatter
-from .formats.html import HTMLTableFormatter
+def create_formatter(name, column_formats: list | None = None, upper_headers: bool | None = None):
+    if name not in TableFormatter._formats:
+        __import__(f'{__package__}.formats.{name}')
+    formatter_cls = TableFormatter._formats.get(name)
+    if not formatter_cls:
+        raise RuntimeError('Unknown format %s' % name)
 
-def create_formatter(format: str, column_formats: list | None = None, upper_headers: bool | None = None):
-    if format == 'text':
-        formatter_cls = TextTableFormatter
-    elif format == 'csv':
-        formatter_cls = CSVTableFormatter
-    elif format == 'html':
-        formatter_cls = HTMLTableFormatter
-    else:
-        return NotImplementedError
-
-    if column_formats is not None:
+    if column_formats:
         class formatter_cls(ColumnFormatMixin, formatter_cls):
             formats = column_formats
+
     if upper_headers:
         class formatter_cls(UpperHeadersMixin, formatter_cls):
             pass
